223d69deedd508be1326fafb4ac7fb5bf68ce8b0
samina <you@example.com>
2023-04-11 22:33:55 +0500

    contactform

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php 
+
+$name = $_REQUEST['userName'];
+$email = $_REQUEST['userEmail'];
+$subject = $_REQUEST['subject'];
+$content = $_REQUEST['content'];
+if(empty($name)|| empty($email) || empty($subject) || empty($contact)) {
+
+                            echo "please fill all fields";
+                        
+                            
+                        
+                        }
+else
+{
+    mail("saminaparveen31@gmail.com","User messege" , $content,"From:$name<$email>");
+    echo "<script type='text/javascript'>alert(''your messege sent succussfully);
+    window.history.log(-1)
+    </script>";
+}                        
+?>
