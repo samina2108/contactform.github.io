f2ff041694dc5d11cf0fa1c348192908468fde8f
samina <you@example.com>
2023-04-12 00:06:29 +0500

    change code

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ $name = $_REQUEST['userName'];
 $email = $_REQUEST['userEmail'];
 $subject = $_REQUEST['subject'];
 $content = $_REQUEST['content'];
-if(empty($name)|| empty($email) || empty($subject) || empty($contact)) {
+if(empty($name)|| empty($email) || empty($subject) || empty($content)) {
 
                             echo "please fill all fields";
                         
@@ -13,9 +13,9 @@ if(empty($name)|| empty($email) || empty($subject) || empty($contact)) {
                         }
 else
 {
-    mail("saminaparveen31@gmail.com","User messege" , $content,"From:$name<$email>");
-    echo "<script type='text/javascript'>alert(''your messege sent succussfully);
-    window.history.log(-1)
+    mail("saminaparveen31@gmail.com","User messege" , $content,"From: $name<$email>");
+    echo "<script type='text/javascript'>alert('your messege sent succussfully');
+    window.history.log(-1);
     </script>";
 }                        
 ?>
